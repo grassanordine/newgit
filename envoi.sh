9723f68be38ed6dfc8a489639f06e67db7df08cc
NetAcad DEVASC <changeme@example.com>
2021-09-19 12:31:29 +0000

    first commit

---
+++ envoi.sh
@@ -0,0 +1,7 @@
+#git remote add origin https://github.com/ngrassa/newgit.git
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/grassanordine/newgit.git
+git push -u origin main
